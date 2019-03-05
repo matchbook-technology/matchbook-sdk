4ffa0cc2f6166a9641567826faad74e7632badde
Sergii Volkodav <volkodav_s@yahoo.com>
2019-03-05 22:28:50 +0000

    add basic impl for login request

--- src/main/java/com/matchbook/sdk/clients/rest/UserResource.java
+++ src/main/java/com/matchbook/sdk/clients/rest/UserResource.java
@@ -1,13 +1,14 @@
 package com.matchbook.sdk.clients.rest;
 
+import com.matchbook.sdk.StreamObserver;
 import com.matchbook.sdk.clients.rest.dtos.user.LoginRequest;
 import com.matchbook.sdk.clients.rest.dtos.user.LoginResponse;
 import com.matchbook.sdk.clients.rest.dtos.user.LogoutResponse;
 
 public interface UserResource {
 
-    LoginResponse login(LoginRequest loginRequest);
+    void login(LoginRequest loginRequest, StreamObserver<LoginResponse> response);
 
-    LogoutResponse logout(String sessionToken);
+    void logout(String sessionToken, StreamObserver<LogoutResponse> response);
 
 }
