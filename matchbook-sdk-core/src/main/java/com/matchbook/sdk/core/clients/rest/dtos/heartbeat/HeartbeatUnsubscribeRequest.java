95d66192699dfe37d255ba898c3cca2ea22215c6
Matteo Dri <mdri@xanadu.ie>
2019-07-12 17:00:03 +0200

    Replace emptyMap object

--- matchbook-sdk-core/src/main/java/com/matchbook/sdk/core/clients/rest/dtos/heartbeat/HeartbeatUnsubscribeRequest.java
+++ matchbook-sdk-core/src/main/java/com/matchbook/sdk/core/clients/rest/dtos/heartbeat/HeartbeatUnsubscribeRequest.java
@@ -14,6 +14,6 @@ public class HeartbeatUnsubscribeRequest implements RestRequest {
 
     @Override
     public Map<String, String> parameters() {
-        return Collections.EMPTY_MAP;
+        return Collections.emptyMap();
     }
 }
