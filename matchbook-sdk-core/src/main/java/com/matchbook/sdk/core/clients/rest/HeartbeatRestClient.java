fa1fa96d61995c1ba26d50fadf082b31b99d9e0c
Alessandro Dal Bello <adbello@xanadu.ie>
2019-07-12 16:42:25 +0200

    Implement read-only offers REST API's

--- matchbook-sdk-core/src/main/java/com/matchbook/sdk/core/clients/rest/HeartbeatRestClient.java
+++ matchbook-sdk-core/src/main/java/com/matchbook/sdk/core/clients/rest/HeartbeatRestClient.java
@@ -7,10 +7,12 @@ import com.matchbook.sdk.core.clients.rest.dtos.heartbeat.HeartbeatSendRequest;
 import com.matchbook.sdk.core.clients.rest.dtos.heartbeat.HeartbeatUnsubscribeRequest;
 
 public interface HeartbeatRestClient {
+
     void getHeartbeat(HeartbeatGetRequest heartbeatGetRequest, StreamObserver<Heartbeat> heartbeatObserver);
 
     void sendHeartbeat(HeartbeatSendRequest heartbeatSendRequest, StreamObserver<Heartbeat> heartbeatObserver);
 
     void unsubscribeHeartbeat(HeartbeatUnsubscribeRequest heartbeatUnsubscribeRequest,
         StreamObserver<Heartbeat> heartbeatObserver);
+
 }
