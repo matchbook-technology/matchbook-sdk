fe87882579af84a7953799b92b342f761895ba0f
Matteo Dri <mdri@xanadu.ie>
2019-08-12 10:19:50 +0200

    Put comment in empty method body

--- matchbook-sdk-rest/src/test/java/com/matchbook/sdk/rest/clients/rest/UserClientRestTest.java
+++ matchbook-sdk-rest/src/test/java/com/matchbook/sdk/rest/clients/rest/UserClientRestTest.java
@@ -348,6 +348,7 @@ public class UserClientRestTest extends MatchbookSDKClientTest {
         return new StreamObserver<T>() {
             @Override
             public void onNext(T response) {
+                // do nothing
             }
 
             @Override
