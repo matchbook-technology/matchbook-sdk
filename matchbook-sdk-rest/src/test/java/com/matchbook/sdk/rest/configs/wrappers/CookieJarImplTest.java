70aa9dc0db7b1133d6c1f04358648f113fb11e7f
Matteo Dri <mdri@xanadu.ie>
2019-09-20 18:37:08 +0200

    Add mb-client-type=mb-sdk cookie to all rest requests

---
+++ matchbook-sdk-rest/src/test/java/com/matchbook/sdk/rest/configs/wrappers/CookieJarImplTest.java
@@ -0,0 +1,52 @@
+package com.matchbook.sdk.rest.configs.wrappers;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.HttpUrl;
+
+public class CookieJarImplTest {
+
+    private CookieJar target = new SDKCookieJar();
+
+    @Test
+    public void saveFromResponseAndLoadForRequestTest() {
+        HttpUrl httpUrl = HttpUrl.get("http://mydomain.com");
+        List<Cookie> cookies = new ArrayList<>();
+        Cookie cookie1 = createCookie("cookie1", "value1");
+        Cookie cookie2 = createCookie("cookie2", "value2");
+        cookies.add(cookie1);
+        cookies.add(cookie2);
+
+        target.saveFromResponse(httpUrl, cookies);
+
+        List<Cookie> requestCookies = target.loadForRequest(httpUrl);
+        assertNotNull(requestCookies);
+        assertEquals(3, requestCookies.size());
+
+        verifyCookieMatch("mb-client-type", "mb-sdk", requestCookies.get(0));
+        verifyCookieMatch(cookie1.name(), cookie1.value(), requestCookies.get(1));
+        verifyCookieMatch(cookie2.name(), cookie2.value(), requestCookies.get(2));
+    }
+
+    private Cookie createCookie(String name, String value) {
+        return new Cookie.Builder()
+            .path("/")
+            .domain("*")
+            .name(name)
+            .value(value)
+            .build();
+    }
+
+    private void verifyCookieMatch(String expectedName, String expectedValue, Cookie cookie) {
+        assertEquals(expectedName, cookie.name());
+        assertEquals(expectedValue, cookie.value());
+    }
+}
